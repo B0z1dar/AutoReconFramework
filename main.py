1db70b848a37ea74cad44851df6cb12dd3a06513
B0z1dar <gusihinbogdan4@gmail.com>
2022-11-08 18:17:35 +0300

    add struct

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import argparse
 from report_parser import masscan_parser
 from configurator import config
+from recon import port_finder
 
 
 class ArgumentParser:
     def __int__(self):
         pass
 
+
+"""
+
+class CommandHandler:
+    pass
+
+class UserValueChecker:
+    pass
+    
+"""
+user_set_rate = "500"
+user_set_target = "192.168.0.1"
+
+new_config = config.MasScanConfig(masscan_rate=user_set_rate, masscan_target=user_set_rate)
+
+new_scan = port_finder.MasScanStarter(user_config=new_config)
 
 if __name__ == '__main__':
     pass
--- recon/port_finder.py
+++ recon/port_finder.py
@@ -1,7 +1,12 @@
 from configurator import config
 
-"""
 
-masscan logic
+class MasScanStarter:
+    def __init__(self, user_config: config.MasScanConfig):
+        self.config = user_config
+        self.target = self.config.get_targets()
+        self.rate = self.config.get_rate()
 
-"""
+    def start_scan(self):
+        f"""sudo masscan -p1-65535 --{self.rate} --open-only {self.target} -oJ ..\\reports\\scan_result.json"""
+
